0c11d03f2167d61cba6d957f0f99ac350fa1145b
qcalmel <quentin.calmel@gmail.com>
2021-04-20 18:18:50 +0200

    Update fixtures

--- fixtures/fixtures.js
+++ fixtures/fixtures.js
@@ -2,14 +2,20 @@ const db = require("../models");
 
 const sequelizeFixtures = require('sequelize-fixtures');
 
-db.sequelize.sync({force: true}).then(() => {
-    console.log("Drop and re-sync db.");
-    sequelizeFixtures.loadFile(
-        './fixtures/*.json'
-        , db)
-        .then(() => {
-            console.log("Fixtures Loaded");
-            process.exit()
-        });
-
-});
+// db.sequelize.sync({force: true}).then(() => {
+//     console.log("Drop and re-sync db.");
+//     sequelizeFixtures.loadFile(
+//         './fixtures/*.json'
+//         , db)
+//         .then(() => {
+//             console.log("Fixtures Loaded");
+//             process.exit()
+//         });
+// });
+sequelizeFixtures.loadFile(
+    './fixtures/*.json'
+    , db)
+    .then(() => {
+        console.log("Fixtures Loaded");
+        process.exit()
+    });
